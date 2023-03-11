7bba32a8a06713e4585094748dd91e1505cac230
jessethomascs <56457691+jessethomascs@users.noreply.github.com>
2023-03-11 02:46:48 -0500

    Start of website for web app

---
+++ README.md
@@ -0,0 +1,4 @@
+# dnd-web-app
+This is a fully fledged applicational website which will probably devolve into other things like a full blown desktop application with phone app image scanning
+
+end my existence
---
+++ test_project/src/App.vue
@@ -0,0 +1,20 @@
+<template>
+  <div class="page">
+    <div class="ribbon">
+      Online DND Simulation v.0.1
+    </div>
+    <div class="ribbonButtons">
+      <button id="ribbonHome">Home</button>
+
+      <button id="ribbonAboutMe">About Me</button>
+
+      <button id="ribbonSimulator">Simulator</button>
+    </div>
+  </div>
+</template>
+
+
+<style scoped src="./App.css">
+/* @import '../assets/css/startpage.css'; */
+  @import './App.css';
+</style>
